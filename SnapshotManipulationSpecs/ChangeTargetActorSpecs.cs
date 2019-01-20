c954a1c13559ca796e9d550e3911c6f2aaa95ce0
Aaron Stannard <aaron@petabridge.com>
2019-01-20 03:44:36 -0800

    fixed issue with spec

--- SnapshotManipulationSpecs/ChangeTargetActorSpecs.cs
+++ SnapshotManipulationSpecs/ChangeTargetActorSpecs.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Akka.Actor;
 using Akka.TestKit.Xunit2;
+using FluentAssertions;
 using Xunit;
 
 namespace SnapshotManipulationSpecs
@@ -24,6 +26,12 @@ namespace SnapshotManipulationSpecs
             changeTarget1.Tell(new DeliverTo(testActorPath, "hit2"));
             changeTarget1.Tell(new DeliverTo(testActorPath, "hit3"));
             ReceiveN(3); // wait to receive all 3 of the message delivery attempts
+            probe1.ExpectNoMsg(500); // shouldn't receive any messages
+            changeTarget1.Tell(ForceRestart.Instance);
+
+            // need to wait 6 seconds so the 5 second re-delivery interval has a chance to fire first
+            var messages = probe1.ReceiveN(3, TimeSpan.FromSeconds(6)).Cast<ConfirmableDelivery>().Select(x => x.Message).ToArray();
+            messages.Should().BeEquivalentTo("hit1", "hit2", "hit3");
         }
     }
 }
