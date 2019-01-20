dd1f105059f3f6ba45e23d9107f1e6873e7aa239
Aaron Stannard <aaron@petabridge.com>
2019-01-20 03:09:54 -0800

    added actors

---
+++ SnapshotManipulationSpecs/Actors/AtLeastOnceDeliveryChangeTargetActor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Akka.Actor;
+using Akka.Persistence;
+
+namespace SnapshotManipulationSpecs
+{
+    public class AtLeastOnceDeliveryChangeTargetActor : AtLeastOnceDeliveryReceiveActor
+    {
+        private readonly ActorPath _updatedTarget;
+        private Predicate<UnconfirmedDelivery> _useNewTarget;
+
+        public AtLeastOnceDeliveryChangeTargetActor(string persistenceId, ActorPath updatedTarget, Predicate<UnconfirmedDelivery> useNewTarget)
+        {
+            PersistenceId = persistenceId;
+            _updatedTarget = updatedTarget;
+            _useNewTarget = useNewTarget;
+
+            Recover<SnapshotOffer>(so =>
+            {
+                if (so.Snapshot is AtLeastOnceDeliverySnapshot ald)
+                {
+                    /*
+                     * Manipulate delivery snapshot by changing the delivery dates to
+                     * something immediate, rather than long-term in the future.                     *
+                     */
+                    var undeliverableMessages = ald.UnconfirmedDeliveries.
+                    Select(x =>
+                    {
+                        // change delivery to new target ActorPath
+                        if (_useNewTarget(x))
+                        {
+                            return new UnconfirmedDelivery(x.DeliveryId, _updatedTarget, x.Message);
+                        }
+
+                        // use the original actor if the predicate is false
+                        return x;
+                    }).ToArray();
+
+                    // create a new AtLeastOnceDeliverySnapshot object
+                    var snapshot = new AtLeastOnceDeliverySnapshot(ald.CurrentDeliveryId, undeliverableMessages);
+
+                    // startup this actor with the updated snapshot
+                    SetDeliverySnapshot(snapshot);
+                }
+            });
+
+            Command<DeliverTo>(d =>
+            {
+                Deliver(d.Target, l => new ConfirmableDelivery(l, d.Message));
+                SaveSnapshot(GetDeliverySnapshot());
+            });
+            Command<Confirmed>(c => { ConfirmDelivery(c.DeliveryId); SaveSnapshot(GetDeliverySnapshot()); });
+            Command<GetDeliveryState>(_ => Sender.Tell(GetDeliverySnapshot()));
+            Command<SaveSnapshotSuccess>(success =>
+            {
+                // delete prior snapshots
+                DeleteSnapshots(new SnapshotSelectionCriteria(success.Metadata.SequenceNr-1));
+            });
+        }
+
+        public override string PersistenceId { get; }
+    }
+}
---
+++ SnapshotManipulationSpecs/Actors/Messages.cs
@@ -0,0 +1,54 @@
+﻿using Akka.Actor;
+using Akka.Persistence;
+
+namespace SnapshotManipulationSpecs
+{
+    /// <summary>
+    /// Retrieves a copy of the <see cref="AtLeastOnceDeliverySnapshot"/>
+    /// from the current <see cref="AtLeastOnceDeliveryActor"/> instance.
+    /// </summary>
+    public sealed class GetDeliveryState
+    {
+        public static readonly GetDeliveryState Instance = new GetDeliveryState();
+        private GetDeliveryState() { }
+    }
+
+    /// <summary>
+    /// Used to guide delivery of specific messages for certain tests
+    /// </summary>
+    public sealed class DeliverTo
+    {
+        public DeliverTo(ActorPath target, object message)
+        {
+            Target = target;
+            Message = message;
+        }
+
+        public ActorPath Target { get; }
+
+        public object Message { get; }
+    }
+
+    public sealed class ConfirmableDelivery
+    {
+        public ConfirmableDelivery(long deliveryId, object message)
+        {
+            DeliveryId = deliveryId;
+            Message = message;
+        }
+
+        public long DeliveryId { get; }
+
+        public object Message { get; }
+    }
+
+    public sealed class Confirmed
+    {
+        public Confirmed(long deliveryId)
+        {
+            DeliveryId = deliveryId;
+        }
+
+        public long DeliveryId { get; }
+    }
+}
